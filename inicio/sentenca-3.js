3649ea9957daf362581a2522aef9529680b491d5
samuelfarias-e1 <samuelcontatoe1@gmail.com>
2025-04-10 02:02:25 -0300

    Add Pasta inicio

---
+++ inicio/sentenca-3.js
@@ -0,0 +1,11 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log('Passo #03'); // mais um exemplo de console.log
+// console.log('Passo #01');
+
+/*
+ * Esse é um
+ * comentário
+ * de múltiplas
+ * linhas
+*/
+console.log('Passo #02');
