3679dca573c3d08564ee968490085fe348d6ead5
samuelfarias-e1 <samuelcontatoe1@gmail.com>
2025-04-11 01:12:21 -0300

    second commit atualizado até aula 26

---
+++ inicio/desafio_sentanca_bloco.js
@@ -0,0 +1,22 @@
+{
+    // bloco 1 impares
+console.log(1)
+console.log(3)
+console.log(5)
+console.log(7)
+console.log(9)
+}
+
+{
+//espaço
+console.log()
+}
+
+{
+    //bloco dois pares
+console.log(2)
+console.log(4)
+console.log(6)
+console.log(8)
+console.log(10)
+}
